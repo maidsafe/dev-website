604054e7ac38afd1d0d19996681bdf25ea9d50c6
hunterlester <guilfordhunter@gmail.com>
2018-04-04 05:41:13 -0700

    MAID-2610 feat/all: initial commit of React-Static, dir organisation for i18n, safe_ux_guidelines

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+
+gulp.task("safe-ux", () => {
+  gulp.src([
+    './node_modules/safe_ux_guidelines/assets/**/*',
+  ])
+  .pipe(gulp.dest('./src/assets'));
+
+  gulp.src([
+    './node_modules/safe_ux_guidelines/scss/**/*',
+  ])
+  .pipe(gulp.dest('./src/scss'));
+});
