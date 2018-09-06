67ed7377f21423c03aaaab0819b196fc92d9250a
Lionel Faber <lionel1704@gmail.com>
2018-09-06 18:20:59 +0530

    fix/deps: Update vulnerable deps and fix development server

--- static.config.js
+++ static.config.js
@@ -41,9 +41,11 @@ export default {
         ],
       },
     ]
-    config.plugins = [
-      new ExtractTextPlugin('styles.css'),
-    ]
+    if(stage != 'dev') {
+      config.plugins = [
+        new ExtractTextPlugin('styles.css')
+      ]
+    }
     return config
   },
   Document: ({ Html, Head, Body, children, siteData, renderMeta }) => (
