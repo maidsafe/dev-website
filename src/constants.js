f5d1b0ff34d15bdd06244b097a8b9ddd58607b8e
Nadia Burborough <nadia.burborough@maidsafe.net>
2018-09-12 14:12:13 +0100

    refactor/weblink: updated web hosting manger link to new repo

--- src/constants.js
+++ src/constants.js
@@ -15,7 +15,7 @@ export default {
     },
     appDevs: {
       sampleApps: {
-        webHosting: 'https://github.com/maidsafe/safe_examples/tree/master/web_hosting_manager/',
+        webHosting: 'https://github.com/maidsafe/safe-web-hosting-manager-electron/releases/',
         emailApp: 'https://github.com/maidsafe/safe_examples/tree/master/email_app/',
         jams: 'https://safenetforum.org/t/introducing-jams-demo-alpha-2/21373',
         safeCms: 'https://safenetforum.org/t/safe-cms-censorship-is-a-thing-of-the-past/'
