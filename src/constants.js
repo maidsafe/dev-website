400f1a7298b2774271ffb5745f744dc7ab70b1dc
David Geddes <david.geddes@maidsafe.net>
2018-09-19 17:19:32 +0100

    Change formatting of PARSEC page
    Removed the logo images for the resources and changed to simple links.
    Also tidied formatting so all links are improved.
    TODO: centre align the links!

--- src/constants.js
+++ src/constants.js
@@ -20,14 +20,6 @@ export default {
         jams: 'https://safenetforum.org/t/introducing-jams-demo-alpha-2/21373',
         safeCms: 'https://safenetforum.org/t/safe-cms-censorship-is-a-thing-of-the-past/'
       }
-    },
-    parsec: {
-      resources: {
-        github: 'https://github.com/maidsafe/parsec',
-        rfc: 'https://github.com/maidsafe/rfcs/blob/master/text/0049-parsec/0049-parsec.md',
-        whitepaper: 'http://docs.maidsafe.net/Whitepapers/pdf/PARSEC.pdf',
-        documentation: 'https://docs.rs/parsec/'
-      }
     }
   },
   locals: {
--- src/containers/parsec.js
+++ src/containers/parsec.js
@@ -20,124 +20,85 @@ import DocumentationLogo from '../../public/images/logo_documentation.png';
 class Parsec extends React.Component {
   constructor() {
     super();
-    this.resLogos = {
-      GITHUB: GithubLogo,
-      RFC: RfcLogo,
-      WHITEPAPER: WhitepapersLogo,
-      DOCUMENTATION: DocumentationLogo
-    };
-    this.resLinks = CONST.externalLinks.parsec.resources;
   }
   render() {
     const { data } = this.props;
 
     return (
       <section className="parsec">
-        <div className="intro base">
+        <div className="intro-base">
 
           <h3 className="title">{data.pageIntro.title}</h3>
           <p className="desc">{data.pageIntro.desc}</p>
+        </div>
 
-          <div className="parsec-i">
-            <div className="parsec-i-b">
-
-              <div className="parsec-i-ctx">
-                <h3 className="parsec-i-h">{data.networkUse.title}</h3>
-                <p className="parsec-i-desc">{data.networkUse.para1}</p>
-                <p className="parsec-i-desc">{data.networkUse.para2}</p>
-              </div>
-            </div>
-          </div>
-          <div className="parsec-i">
-            <div className="parsec-i-b">
-
-              <div className="parsec-i-ctx">
-                <h3 className="parsec-i-h">{data.bigDeal.title}</h3>
-                <p className="parsec-i-desc">{data.bigDeal.para1}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="parsec-i">
-            <div className="parsec-i-b">
-
-              <div className="parsec-i-ctx">
-                <h3 className="parsec-i-h">{data.myProject.title}</h3>
-                <p className="parsec-i-desc">{data.myProject.para1}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="parsec-i">
-            <div className="parsec-i-b">
-
-              <div className="parsec-i-ctx">
-                <h3 className="parsec-i-h">{data.moreInfo.title}</h3>
-                <p className="parsec-i-desc">
-                  {data.moreInfo.para1.chunk1}
-                  <a href={data.moreInfo.link1.href} target="_blank">{data.moreInfo.link1.name}</a>
-                  {data.moreInfo.para1.chunk2}
-                  <a href={data.moreInfo.link2.href} target="_blank">{data.moreInfo.link2.name}</a>
-                  {data.moreInfo.para1.chunk3}
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="parsec-i">
-            <div className="parsec-i-b">
-              <div className="parsec-i-ctx">
-                <h3 className="parsec-i-h">{data.moreInfo.links.title}</h3>
-                <p><a href={data.moreInfo.links.link1.href} target="_blank">{data.moreInfo.links.link1.name}</a></p> &nbsp;
-                <p><a href={data.moreInfo.links.link2.href} target="_blank">{data.moreInfo.links.link2.name}</a></p> &nbsp;
-              </div>
-            </div>
-          </div>
-
-          <div className="parsec-i">
-            <div className="parsec-i-b">
-              <div className="parsec-i-ctx">
-                <h3 className="parsec-i-h">{data.moreInfo.videos.title}</h3>
-                  <p><a href={data.moreInfo.videos.video1.href} target="_blank">{data.moreInfo.videos.video1.name}</a></p> &nbsp;
-                  <p><a href={data.moreInfo.videos.video2.href} target="_blank">{data.moreInfo.videos.video2.name}</a></p> &nbsp;
-                  <p><a href={data.moreInfo.videos.video3.href} target="_blank">{data.moreInfo.videos.video3.name}</a></p> &nbsp;
-                  <p><a href={data.moreInfo.videos.video4.href} target="_blank">{data.moreInfo.videos.video4.name}</a></p> &nbsp;
-
-              </div>
-            </div>
+        <div className="parsec-i">
+          <div className="parsec-i-head"> <h3>{data.networkUse.title}</h3> </div>
+          <div className="parsec-i-cntr">
+            <p className="parsec-i-desc">{data.networkUse.para1}</p>
+            <p className="parsec-i-desc">{data.networkUse.para2}</p>
           </div>
         </div>
 
-        <div className="parres">
-          <div className="parres-b layout4060">
-            <div className="parres-lay-40">
-              <div className="dual-tone-text">
-                <span className="light">{data.resources.title.light1}</span>
-              </div>
-            </div>
-            <div className="parres-lay-60">
-              <div className="parres-cntr">
-                <div className="parres-cntr-b">
-
-                  <div className="parres-app">
-                    <div className="parres-app-ls">
-                      {
-                        data.resources.res.map((res, i) => (
-                          <div key={`parres-app-${i}`} className="parres-app-i">
-                            <a href={this.resLinks[res.linkId]} target="_blank">
-                              <div className="parres-app-logo"><img src={this.resLogos[res.logo]} /></div>
-                              <div className="parres-app-name">{res.name}</div>
-                            </a>
-                          </div>
-                        ))
-                      }
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div className="parsec-i">
+          <div className="parsec-i-head"> <h3>{data.bigDeal.title}</h3> </div>
+          <div className="parsec-i-cntr">
+            <p>
+              {data.bigDeal.para1}
+            </p>
           </div>
         </div>
+
+        <div className="parsec-i">
+          <div className="parsec-i-head"> <h3>{data.myProject.title}</h3> </div>
+          <div className="parsec-i-cntr">
+            <p>
+              {data.myProject.para1}
+            </p>
+          </div>
+        </div>
+
+        <div className="parsec-i">
+          <div className="parsec-i-head"> <h3>{data.moreInfo.title}</h3> </div>
+          <div className="parsec-i-cntr">
+            <p>
+              {data.moreInfo.para1.chunk1}
+              <a href={data.moreInfo.link1.href} target="_blank">{data.moreInfo.link1.name}</a>
+              {data.moreInfo.para1.chunk2}
+              <a href={data.moreInfo.link2.href} target="_blank">{data.moreInfo.link2.name}</a>
+              {data.moreInfo.para1.chunk3}
+            </p>
+          </div>
+        </div>
+
+        <div className="parsec-i">
+          <div className="parsec-i-head"> <h3>{data.resources.title}</h3> </div>
+          <div className="parsec-i-cntr">
+            <p><a href={data.resources.res1.href} target="_blank">{data.resources.res1.name}</a></p>
+            <p><a href={data.resources.res2.href} target="_blank">{data.resources.res2.name}</a></p>
+            <p><a href={data.resources.res3.href} target="_blank">{data.resources.res3.name}</a></p>
+            <p><a href={data.resources.res4.href} target="_blank">{data.resources.res4.name}</a></p>
+          </div>
+        </div>
+
+        <div className="parsec-i">
+          <div className="parsec-i-head"> <h3>{data.articles.title}</h3> </div>
+          <div className="parsec-i-cntr">
+            <p><a href={data.articles.link1.href} target="_blank">{data.articles.link1.name}</a></p>
+            <p><a href={data.articles.link2.href} target="_blank">{data.articles.link2.name}</a></p>
+          </div>
+        </div>
+
+        <div className="parsec-i">
+          <div className="parsec-i-head"> <h3>{data.videos.title}</h3> </div>
+          <div className="parsec-i-cntr">
+            <p><a href={data.videos.video1.href} target="_blank">{data.videos.video1.name}</a></p>
+            <p><a href={data.videos.video2.href} target="_blank">{data.videos.video2.name}</a></p>
+            <p><a href={data.videos.video3.href} target="_blank">{data.videos.video3.name}</a></p>
+            <p><a href={data.videos.video4.href} target="_blank">{data.videos.video4.name}</a></p>
+          </div>
+        </div>
+
       </section>
     )
   }
